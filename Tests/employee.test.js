80aad6c1619e246748bf727ffc3a85ad0e113c06
Sheri Foster <87589967+SFosR@users.noreply.github.com>
2021-12-08 12:21:40 -0500

    got employee.test.js to pass

--- Tests/employee.test.js
+++ Tests/employee.test.js
@@ -1,32 +1,32 @@
-const { test, expect } = require('@jest/globals')
-const Employee = require('../lib/Employee')
+const Employee = require("../lib/Employee");
 
-test('test employee object', ()=> {
-    const worker = new Employee();
-    expect(typeof(worker)).toBe("object");
+describe("Employee", () => {
+  it("Creates Employee", () => {
+    const employee = new Employee("Sheri", "1", "fostr.sheri@gmail.com");
+
+    expect(employee.name).toBe("Sheri");
+    expect(employee.id).toBe("1");
+    expect(employee.email).toBe("fostr.sheri@gmail.com");
+    expect(employee.type).toBe("Employee");
+  });
+
+  it("sets the name", () => {
+    const employee = new Employee("Sheri", "1", "fostr.sheri@gmail.com");
+    expect(employee.getName()).toBe("Sheri");
+  });
+
+  it("sets the Id", () => {
+    const employee = new Employee("Sheri", "1", "fostr.sheri@gmail.com");
+    expect(employee.getId()).toBe("1");
+  });
+
+  it("sets the email", () => {
+    const employee = new Employee("Sheri", "1", "fostr.sheri@gmail.com");
+    expect(employee.getEmail()).toBe("fostr.sheri@gmail.com");
+  });
+
+  it("sets the Role", () => {
+    const employee = new Employee("Sheri", "1", "fostr.sheri@gmail.com");
+    expect(employee.getRole()).toBe("Employee");
+  });
 });
-
-it("Sets the name", ()=> {
-    const testName = "Frank";
-    const worker = new Employee(testName);
-    expect(worker.name).toBe(testName);
-});
-
-it("Sets the Id", ()=> {
-    const testId = "10";
-    const worker = new Employee("Frank", testId);
-    expect(worker.id).toBe(testId);
-});
-
-it("Sets the email", ()=> {
-    const testEmail = "frank@frank.com";
-    const worker = new Employee("Frank", 10, testEmail);
-    expect(worker.email).toBe(testEmail);
-});
-
-it("Sets the Role", ()=> {
-    const testRole = "Intern";
-    const worker = new Employee("Frank", 10, "frank@frank.com", testRole);
-    expect(worker.testRole).toBe(testRole);
-});
-
